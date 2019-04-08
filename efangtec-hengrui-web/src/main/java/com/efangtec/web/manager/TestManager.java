1e9d1d32eb3a5320d4ed5a7553b507552de81cc4
wangyafei <772173722@qq.com>
2019-04-08 10:38:26 +0800

    添加manager层

--- efangtec-hengrui-web/src/main/java/com/efangtec/web/manager/TestManager.java
+++ efangtec-hengrui-web/src/main/java/com/efangtec/web/manager/TestManager.java
@@ -1,12 +1,30 @@
 package com.efangtec.web.manager;
 
 
+import com.efangtec.apply.service.AnjinApplyService;
 import com.efangtec.apply.service.HengRuiApplyService;
+import com.efangtec.common.pojo.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Component
 public class TestManager {
+    //@Qualifier标注具体要注入哪个实现类
     @Autowired
-    private HengRuiApplyService hengRuiApplyService;
+    @Qualifier("hengRuiApplyServiceImpl")
+    HengRuiApplyService hengRuiApplyService;
+
+    @Autowired
+    @Qualifier("anjinApplyServiceImpl")
+    AnjinApplyService anjinApplyService;
+
+    @RequestMapping("/test")
+    public Result test(){
+        hengRuiApplyService.delete();
+        System.out.println("-*************************");
+        anjinApplyService.delete();
+        return null;
+    }
 }
--- efangtec-hengrui-web/src/test/java/com/efangtec/HengruiWebApplicationTests.java
+++ efangtec-hengrui-web/src/test/java/com/efangtec/HengruiWebApplicationTests.java
@@ -1,23 +1,31 @@
-package com.efangtec.web.controller;
+package com.efangtec;
 
 import com.efangtec.apply.service.AnjinApplyService;
 import com.efangtec.apply.service.HengRuiApplyService;
 import com.efangtec.common.pojo.Result;
 import com.efangtec.web.manager.TestManager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/test")
-@RestController
-public class TestController {
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HengruiWebApplicationTests {
 
     @Autowired
     TestManager testManager;
-
+    @Test
     @RequestMapping("/test")
-    public Result test(){
-       return  testManager.test();
+    public void test(){
+        testManager.test();
     }
+
+    @Test
+    public void contextLoads() {
+    }
+
 }
